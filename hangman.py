c0d955491cd72a30798bdb334702430265b303d9
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-06-27 23:03:36 -0400

    stop game loop and anounce winner

--- hangman.py
+++ hangman.py
@@ -93,12 +93,20 @@ lives_ascii = [
 
 word = 'empanada'
 blank_line = []
+# add a guessed list to tell the user when a letter was already guessed
 play_game = True
+lives = 8
+
+print("WELCOME TO HANGMAN")
 for _ in range(len(word)):
     blank_line += "_"
 
+print(lives_ascii[0])
+
 while play_game:
+    print(f"Lives: {lives}")
     guess = input("Guess a letter: ")
+
 
     for space_idx in range(len(word)):
         letter = word[space_idx]
@@ -106,5 +114,13 @@ while play_game:
         if letter == guess:
             blank_line[space_idx] = letter
 
-    print(''.join(blank_line))
+    if '_' not in blank_line:
+        play_game = False
+        print('''
+          -------------------------
+          |  YOU WON THE GAME!!!! |
+          -------------------------
+        ''')
+
+    print(' '.join(blank_line))
 
